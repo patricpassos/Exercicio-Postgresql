d3231f2b346079ecfe04a3df0be3be11b063e063
patricpassos <patrickpassos93@gmail.com>
2026-06-30 20:39:45 -0300

    Correção do banco de dados atualizado data: TIMESTAMP WITH TIME ZONE

--- Evento/Evento.sql
+++ Evento/Evento.sql
@@ -43,8 +43,8 @@ INSERT INTO tb_atividade(nome, descricao, preco, categoria_id) VALUES('Curso de 
 INSERT INTO tb_atividade(nome, descricao, preco, categoria_id) VALUES('Oficina de GitHub', 'Controle versões de seus projetos', 50.0, 2);
 
 INSERT INTO tb_bloco(inicio, fim, atividade_id) VALUES('2017-09-25 08:00:00', '2017-09-25 11:00:00', 1);
-UPDATE tb_bloco SET inicio = TIMESTAMP WITH TIME ZONE '2017-09-25T08:00:00Z' WHERE id = 1;
-UPDATE tb_bloco SET fim = TIMESTAMP WITH TIME ZONE '2017-09-25T11:00:00Z' WHERE id = 1;
+UPDATE tb_bloco SET inicio = TIMESTAMP WITH TIME ZONE '2017-09-25T11:00:00Z' WHERE id = 1;
+UPDATE tb_bloco SET fim = TIMESTAMP WITH TIME ZONE '2017-09-25T14:00:00Z' WHERE id = 1;
 INSERT INTO tb_bloco(inicio, fim, atividade_id) VALUES(TIMESTAMP WITH TIME ZONE '2017-09-25T14:00:00Z', TIMESTAMP WITH TIME ZONE '2017-09-25T18:00:00Z', 2);
 INSERT INTO tb_bloco(inicio, fim, atividade_id) VALUES(TIMESTAMP WITH TIME ZONE '2017-09-26T08:00:00Z', TIMESTAMP WITH TIME ZONE '2017-09-26T11:00:00Z', 2);
 
@@ -60,3 +60,4 @@ INSERT INTO tb_participacao(participante_id, atividade_id) VALUES (3, 1);
 INSERT INTO tb_participacao(participante_id, atividade_id) VALUES (3, 2);
 INSERT INTO tb_participacao(participante_id, atividade_id) VALUES (4, 2);
 
+SELECT * FROM tb_bloco;
