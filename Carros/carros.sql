f0d05b78798409a8a6bcadf6cfcbd99fcedb4843
patricpassos <patrickpassos93@gmail.com>
2026-07-01 17:24:18 -0300

    criado tabelas do banco de dados, falta criar as tabelas tb_locacao_diaria e tb_locacao_longo_periodo

---
+++ Carros/carros.sql
@@ -0,0 +1,78 @@
+CREATE TABLE tb_categoria (
+	id SERIAL PRIMARY KEY,
+	descricao VARCHAR(20) NOT NULL,
+	preco_diario FLOAT
+);
+
+CREATE TABLE tb_sede (
+	codigo INT PRIMARY KEY,
+	localidade_s FLOAT NOT NULL,
+	localidade_w FLOAT NOT NULL
+);
+
+CREATE TABLE tb_carro (
+	id SERIAL PRIMARY KEY,
+	modelo VARCHAR(20) NOT NULL,
+	placa VARCHAR(12) NOT NULL,
+	cor SMALLINT,
+	ano SMALLINT,
+	data_aquisicao DATE,
+	categoria_id INT NOT NULL,
+	sede_id INT NOT NULL,
+	FOREIGN KEY (categoria_id) REFERENCES tb_categoria(id),
+	FOREIGN KEY (sede_id) REFERENCES tb_sede(codigo)
+);
+
+CREATE TABLE tb_estado (
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE tb_cidade (
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR (20) NOT NULL,
+	estado_id INT NOT NULL,
+	FOREIGN KEY (estado_id) REFERENCES tb_estado(id)
+);
+
+CREATE TABLE tb_endereco (
+	id SERIAL PRIMARY KEY,
+	logradouro VARCHAR (60),
+	numero INT NOT NULL,
+	complemento VARCHAR (30),
+	bairro VARCHAR (20) NOT NULL,
+	cep VARCHAR (12) NOT NULL,
+	cidade_id SMALLINT NOT NULL,
+	FOREIGN KEY (cidade_id) REFERENCES tb_cidade (id)
+);
+
+CREATE TABLE tb_cliente (
+	cpf VARCHAR(12) PRIMARY KEY,
+	nome VARCHAR (30) NOT NULL,
+	email VARCHAR (30) UNIQUE NOT NULL
+);
+
+CREATE TABLE tb_telefone (
+	cpf VARCHAR (12),
+	numero VARCHAR (12),
+	PRIMARY KEY (cpf, numero),
+	FOREIGN KEY (cpf) REFERENCES tb_cliente (cpf)
+);
+
+CREATE TABLE tb_locacao (
+	id SERIAL PRIMARY KEY,
+	instante_locacao TIMESTAMP NOT NULL,
+	instante_devolucao TIMESTAMP NOT NULL,
+	cliente_id VARCHAR(12) NOT NULL,
+	carro_id INT NOT NULL,
+	local_de_retirada_id INT NOT NULL,
+	FOREIGN KEY (cliente_id) REFERENCES tb_cliente (cpf),
+	FOREIGN KEY (carro_id) REFERENCES tb_carro (id),
+	FOREIGN KEY (local_de_retirada_id) REFERENCES tb_sede (codigo)	
+);
+
+
+
+
+
+
