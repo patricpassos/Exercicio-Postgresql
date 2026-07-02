a76f273766d3e6a5893505aa905c8f39d701d957
patricpassos <patrickpassos93@gmail.com>
2026-07-02 16:45:32 -0300

    Criado tabelas e adicionado insert de dados

--- Carros/carros.sql
+++ Carros/carros.sql
@@ -71,8 +71,50 @@ CREATE TABLE tb_locacao (
 	FOREIGN KEY (local_de_retirada_id) REFERENCES tb_sede (codigo)	
 );
 
+CREATE TABLE tb_locacao_diaria (
+	id SERIAL PRIMARY KEY,
+	dias_previsto INT NOT NULL,
+	FOREIGN KEY (id) REFERENCES tb_locacao (id)
+);
 
+CREATE TABLE tb_locacao_longo_periodo (
+	id SERIAL PRIMARY KEY,
+	porcentagem_desconto FLOAT,
+	FOREIGN KEY (id) REFERENCES tb_locacao(id)
+);
 
+-- Inserts de dados
 
+INSERT INTO tb_categoria (descricao, preco_diario) VALUES ('Básico', 60.0);
+INSERT INTO tb_categoria (descricao, preco_diario) VALUES ('Luxo', 150.0);
 
+INSERT INTO tb_sede (codigo, localidade_s, localidade_w) VALUES (1, 18.28272, 23.38474);
 
+INSERT INTO tb_carro (modelo, placa, cor, ano, data_aquisicao, categoria_id, sede_id) VALUES ('Polo', 'HDD9383', 0, 2015, '2015-07-21', 1, 1);
+INSERT INTO tb_carro (modelo, placa, cor, ano, data_aquisicao, categoria_id, sede_id) VALUES ('Fusion', 'PEH3837', 1, 2016, '2016-12-25', 2, 1);
+UPDATE tb_carro SET id = 1 WHERE placa = 'HDD9383';
+UPDATE tb_carro SET id = 2 WHERE placa = 'PEH3837';
+
+INSERT INTO tb_estado (nome) VALUES ('São Paulo');
+
+INSERT INTO tb_cidade (nome, estado_id) VALUES ('São Paulo', 1);
+
+INSERT INTO tb_endereco (logradouro, numero, complemento, bairro, cep, cidade_id) VALUES ('Rua Flores', 205, 'Bloco C', 'Jardim', 38400282, 1);
+
+INSERT INTO tb_cliente (cpf, nome, email) VALUES (93838673610, 'Maria Silva', 'maria@gmail.com');
+INSERT INTO tb_cliente (cpf, nome, email) VALUES (58236392715, 'Joaquim Jorge', 'joaquim@gmail.com');
+
+INSERT INTO tb_telefone (cpf, numero) VALUES (93838673610, 37635393);
+INSERT INTO tb_telefone (cpf, numero) VALUES (58236392715, 37636364);
+INSERT INTO tb_telefone (cpf, numero) VALUES (58236392715, 89988464);
+
+INSERT INTO tb_locacao (instante_locacao, instante_devolucao, cliente_id, carro_id, local_de_retirada_id) VALUES (TIMESTAMP WITH TIME ZONE '2017-09-30T13:34:00Z', TIMESTAMP WITH TIME ZONE '2017-10-04T14:34:00Z', '93838673610', 1, 1);
+INSERT INTO tb_locacao (instante_locacao, instante_devolucao, cliente_id, carro_id, local_de_retirada_id) VALUES (TIMESTAMP WITH TIME ZONE '2017-11-11T12:00:00Z', TIMESTAMP WITH TIME ZONE '2017-11-15T13:00:00Z', '93838673610', 2, 1);
+INSERT INTO tb_locacao (instante_locacao, instante_devolucao, cliente_id, carro_id, local_de_retirada_id) VALUES (TIMESTAMP WITH TIME ZONE '2017-12-25T03:00:00Z', TIMESTAMP WITH TIME ZONE '2018-07-31T03:00:00Z', '58236392715', 2, 1);
+
+INSERT INTO tb_locacao_diaria (dias_previsto) VALUES (4);
+INSERT INTO tb_locacao_diaria (dias_previsto) VALUES (4);
+
+INSERT INTO tb_locacao_longo_periodo (id, porcentagem_desconto) VALUES (3, 50.0)
+
+SELECT * FROM tb_locacao_longo_periodo;
