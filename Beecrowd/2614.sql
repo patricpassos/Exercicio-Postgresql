b0bec376ee567ef4515bb388e157171e13cbeb09
patricpassos <patrickpassos93@gmail.com>
2026-07-10 09:56:19 -0300

    Exercicios sobre consultas SQL

---
+++ Beecrowd/2614.sql
@@ -0,0 +1,62 @@
+--- URI Online Judge SQL
+--- Copyright URI Online Judge
+--- www.urionlinejudge.com.br
+--- Problem 2614
+
+CREATE TABLE customers (
+  id numeric PRIMARY KEY,
+  name varchar(50),
+  street varchar(50),
+  city varchar(50)
+);
+
+CREATE TABLE rentals (
+  id numeric PRIMARY KEY,
+  rentals_date date,
+  id_customers numeric REFERENCES customers (id)
+);
+
+INSERT INTO customers (id, name, street, city)
+VALUES
+  (1,	'Giovanna Goncalves Oliveira',	'Rua Mato Grosso',	'Canoas'),
+  (2,	'Kauã Azevedo Ribeiro',	'Travessa Ibiá',	'Uberlândia'),
+  (3,	'Rebeca Barbosa Santos',	'Rua Observatório Meteorológico',	'Salvador'),
+  (4,	'Sarah Carvalho Correia',	'Rua Antônio Carlos da Silva',	'Apucarana'),
+  (5,	'João Almeida Lima',	'Rua Rio Taiuva',	'Ponta Grossa'),
+  (6,	'Diogo Melo Dias',	'Rua Duzentos e Cinqüenta',	'Várzea Grande');
+  
+INSERT INTO rentals (id, rentals_date, id_customers)
+VALUES
+  (1,	'09/10/2016',	3),
+  (2,	'02/09/2016',	1),
+  (3,	'02/08/2016',	4),
+  (4,	'02/09/2015',	2),
+  (5,	'02/03/2016',	6),
+  (6,	'04/04/2016',	4);
+  
+  /*  Execute this query to drop the tables */
+  -- DROP TABLE rentals, customers; --
+
+SELECT *
+FROM customers;
+
+SELECT *
+FROM rentals;
+
+SELECT *
+FROM rentals
+INNER JOIN customers ON rentals.id_customers = customers.id;
+
+SELECT name, rentals_date
+FROM rentals
+INNER JOIN customers ON rentals.id_customers = customers.id;
+
+SELECT name, rentals_date, CONCAT(EXTRACT(YEAR FROM rentals_date), '-', EXTRACT(MONTH FROM rentals_date))
+FROM rentals
+INNER JOIN customers ON rentals.id_customers = customers.id;
+
+SELECT name, rentals_date
+
+FROM rentals
+INNER JOIN customers ON rentals.id_customers = customers.id
+WHERE CONCAT(EXTRACT(YEAR FROM rentals_date), '-', EXTRACT(MONTH FROM rentals_date)) = '2016-9';
